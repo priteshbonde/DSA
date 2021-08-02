7a4d71c4f1ee61a1929fec44602ef967c935878a
Pritesh Bonde <pbonde@mimeo.com>
2021-08-02 19:12:30 +0530

    Added reverse list and delete skip nodes

--- JS/DoublyLinkedList.js
+++ JS/DoublyLinkedList.js
@@ -10,6 +10,7 @@ class DoublyLinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
     addToBack(value) {
@@ -21,7 +22,8 @@ class DoublyLinkedList {
             this.tail.next = newNode;
             newNode.prev = this.tail;
         }
-        this.tail = newNode
+        this.tail = newNode;
+        this.length += 1;
     }
 
     addToFront(value) {
@@ -35,6 +37,7 @@ class DoublyLinkedList {
         }
 
         this.head = newNode;
+        this.length += 1;
     }
 
     insertAfter(valueToSearch, valueToInsert) {
@@ -76,7 +79,7 @@ class DoublyLinkedList {
                 current.next.prev.prev = current;
                 current.next.prev.next = current.next;
                 current.next = current.next.prev;
-
+                this.length += 1;
                 return true;
 
 
@@ -104,6 +107,7 @@ class DoublyLinkedList {
                 current.prev.next.prev = current.prev;
                 current.prev.next.next = current;
                 current.prev = current.prev.next;
+                this.length += 1;
                 return;
 
             }
@@ -114,6 +118,76 @@ class DoublyLinkedList {
         return;
     }
 
+    deleteNode(startNodeValue, endNodeValue, skipNodes) {
+        
+        let startNode = this.getNode(startNodeValue);
+        let endNode = this.getNode(endNodeValue);
+
+        if(!startNode) {
+            console.error("Start node does not exist");
+            return;
+        }
+
+        if(!endNode) {
+            console.error("End node does not exist");
+            return;
+        }
+
+        let startNodeTemp = startNode;
+        let counter = 0
+        while(startNodeTemp.value != endNodeValue) {
+            if(!startNodeTemp) {
+                counter = -1;
+                break;
+            }
+            startNodeTemp = startNodeTemp.next;
+            counter+=1;
+
+        }
+
+        if(counter<1) {
+            console.error("error while deleting the nodes");
+            return;
+        }
+       
+        while(startNode != endNode) {
+            this.deleteValue(startNode.value);
+            startNode = startNode.next;
+           for (let i = 1; i < skipNodes+1; i = i+1) {
+               if(startNode == endNode) {
+                   break;
+               }
+
+               startNode = startNode.next;
+               
+           }
+
+           if(startNode == endNode) {
+               this.deleteValue(startNode.value);
+           }
+        }
+        
+    }
+
+    reverseList() {
+        for (let current = this.tail; current; current=current.next) {
+            let temp = current.next;
+            current.next = current.prev;
+            current.prev = temp; 
+        }
+        let tempHead = this.tail;
+        this.tail = this.head;
+        this.head = tempHead;
+    }
+
+    getNode(value) {
+        for (let current = this.head; current; current = current.next){
+            if(current.value === value) {
+                return current;
+            }
+           
+        }
+    }
     deleteValue(value) {
 
         if(!this.head) {
@@ -128,6 +202,7 @@ class DoublyLinkedList {
                 if(this.head === this.tail) {
                     this.head = null;
                     this.tail = null;
+                    this.length -= 1;
                     return;
                 }
 
@@ -136,6 +211,7 @@ class DoublyLinkedList {
                     this.head = this.head.next;
                     // current.next.prev = null;
                     this.head.prev = null;
+                    this.length -= 1;
                     return;
                 }
 
@@ -143,22 +219,47 @@ class DoublyLinkedList {
                 if(this.tail === current) {
                     this.tail = this.tail.prev;
                     this.tail.next = null;
+                    this.length -= 1;
                     return;
                 }
                 current.prev.next = current.next;
                 current.next.prev = current.prev;
+                this.length -= 1;
+                return;
             }
             
         }
 
-        console.log("Value not found");
+        console.log(`${value} Value not found`);
+    }
+
+    deleteNodesIndex(start, end, skip) {
+        if(end < start) {
+            console.error("End index must be always greater than start index")
+            return;
+        }
+
+        if(!this.head) {
+            console.error("Head not found");
+            return;
+        }
+
+        let position = start;
+        let index = 1;
+        for (let current = this.head; current; current = current.next) {
+            if(index === position) {
+                this.deleteValue(current.value);
+                position += (skip + 1);
+            } 
+            index+=1;
+        }
     }
 
     printForward() {
         for (let current =  this.head; current ; current = current.next) {
-            console.log(`Previous ${current.prev?.value}, Current value ${current.value}, Next ${current.next?.value}, Type: ${current == this.head? 'Head' : current == this.tail? 'Tail': 'Node'}`);
-            
+            console.log(`Previous ${current.prev?.value}, Current value ${current.value}, Next ${current.next?.value}, Type: ${current == this.head? 'Head' : current == this.tail? 'Tail': 'Node'}`);          
         }
+        console.log(`Length of the Doubly linked list is ${this.length}`);
     }
     printBackward() {
         for (let current = this.tail; current; current = current.prev) {
@@ -169,36 +270,50 @@ class DoublyLinkedList {
 }
 
 let dll = new DoublyLinkedList();
-dll.addToBack(10)
-dll.addToBack(12)
-dll.addToBack(14)
-// dll.printBackward();
+// dll.addToBack(10)
+// dll.addToBack(12)
+// dll.addToBack(14)
+// // dll.printBackward();
 
-// console.log("ForwarPrint");
+// // console.log("ForwarPrint");
+// // dll.printForward();
+
+// dll.addToFront(15)
+// console.log("After add to to front");
 // dll.printForward();
 
-dll.addToFront(15)
-console.log("After add to to front");
-dll.printForward();
+// dll.insertAfter(14, 13);
+// dll.insertAfter(15, 09);
+// console.log("After insert in");
+// dll.printForward();
 
-dll.insertAfter(14, 13);
-dll.insertAfter(15, 09);
-console.log("After insert in");
-dll.printForward();
+// dll.insertBefore(9, 8);
+// console.log("After insert before");
+// dll.printForward();
+// dll.deleteValue(19);
+// dll.deleteValue(15);
+// dll.deleteValue(8);
+// dll.deleteValue(9);
+// dll.deleteValue(10);
+// dll.deleteValue(12);
+// dll.deleteValue(14);
+// dll.deleteValue(13);
 
-dll.insertBefore(9, 8);
-console.log("After insert before");
-dll.printForward();
-dll.deleteValue(19);
-dll.deleteValue(15);
-dll.deleteValue(8);
-dll.deleteValue(9);
-dll.deleteValue(10);
-dll.deleteValue(12);
-dll.deleteValue(14);
-dll.deleteValue(13);
+// console.log("After delete");
+// dll.printForward();
 
+dll.addToBack(10);
+dll.addToBack(23);
+dll.addToBack(34);
+dll.addToBack(45);
+dll.addToBack(56);
+dll.addToBack(67);
+console.log("before Delete");
+dll.printForward()
+// dll.deleteNodesIndex(2,5,1);
 console.log("After delete");
+dll.printForward();
+dll.reverseList();
 dll.printForward();
 
 
